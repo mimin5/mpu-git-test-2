42b8fd37c746a8d81a270d21dc8dfd161fc0224b
azizah khairun nisa <azizahichabts@gmail.com>
2021-05-21 23:09:37 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>coba git remote</title>
 </head>
+
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus deserunt voluptates temporibus accusantium debitis, tempora esse facilis ex deleniti quasi laboriosam corrupti numquam molestiae nisi aut perspiciatis voluptatibus, illo voluptatem!</p>
 </body>
+
 </html>
